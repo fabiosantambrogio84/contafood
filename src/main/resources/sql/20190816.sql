e5b3ed026c2d8e4f9349854c9650d96e9bc2bd5d
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-08-16 16:09:58 +0200

    Fix alter table fornitore

--- src/main/resources/sql/20190816.sql
+++ src/main/resources/sql/20190816.sql
@@ -25,7 +25,7 @@ ALTER TABLE `fornitore` ADD COLUMN id_agente int(10) AFTER id_autista;
 ALTER TABLE `fornitore` ADD COLUMN blocca_ddt bit(1) NOT NULL DEFAULT b'0' AFTER id_agente;
 ALTER TABLE `fornitore` ADD COLUMN nascondi_prezzi bit(1) NOT NULL DEFAULT b'0' AFTER blocca_ddt;
 ALTER TABLE `fornitore` ADD COLUMN raggruppa_riba bit(1) NOT NULL DEFAULT b'0' AFTER nascondi_prezzi;
-ALTER TABLE `fornitore` ADD COLUMN nome_gruppo_riba bit(1) NOT NULL DEFAULT b'0' AFTER raggruppa_riba;
+ALTER TABLE `fornitore` ADD COLUMN nome_gruppo_riba varchar(100) NOT NULL DEFAULT b'0' AFTER raggruppa_riba;
 
 
 
