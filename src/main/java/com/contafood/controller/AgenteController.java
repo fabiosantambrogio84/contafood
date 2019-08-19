741a9a9bdb997bbd045e106c0a2bab5367c9a655
Fabio Santambrogio <f.santambrogio@widegroup.ch>
2019-08-19 11:01:56 +0200

    Add logs

--- src/main/java/com/contafood/controller/AgenteController.java
+++ src/main/java/com/contafood/controller/AgenteController.java
@@ -3,6 +3,8 @@ package com.contafood.controller;
 import com.contafood.exception.CannotChangeResourceIdException;
 import com.contafood.model.Agente;
 import com.contafood.service.AgenteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @RequestMapping(path="/agenti")
 public class AgenteController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(AgenteController.class);
+
     private final AgenteService agenteService;
 
     @Autowired
@@ -27,12 +31,14 @@ public class AgenteController {
     @RequestMapping(method = GET)
     @CrossOrigin
     public Set<Agente> getAll() {
+        LOGGER.info("Performing GET request for retrieving list of 'agenti'");
         return agenteService.getAll();
     }
 
     @RequestMapping(method = GET, path = "/{agenteId}")
     @CrossOrigin
     public Agente getOne(@PathVariable final Long agenteId) {
+        LOGGER.info("Performing GET request for retrieving 'agente' '{}'", agenteId);
         return agenteService.getOne(agenteId);
     }
 
@@ -40,12 +46,14 @@ public class AgenteController {
     @ResponseStatus(CREATED)
     @CrossOrigin
     public Agente create(@RequestBody final Agente agente){
+        LOGGER.info("Performing POST request for creating 'agente'");
         return agenteService.create(agente);
     }
 
     @RequestMapping(method = PUT, path = "/{agenteId}")
     @CrossOrigin
     public Agente update(@PathVariable final Long agenteId, @RequestBody final Agente agente){
+        LOGGER.info("Performing PUT request for updating 'agente' '{}'", agenteId);
         if (!Objects.equals(agenteId, agente.getId())) {
             throw new CannotChangeResourceIdException();
         }
@@ -56,6 +64,7 @@ public class AgenteController {
     @ResponseStatus(NO_CONTENT)
     @CrossOrigin
     public void delete(@PathVariable final Long agenteId){
+        LOGGER.info("Performing DELETE request for deleting 'agente' '{}'", agenteId);
         agenteService.delete(agenteId);
     }
 }
--- src/main/java/com/contafood/service/AgenteService.java
+++ src/main/java/com/contafood/service/AgenteService.java
@@ -3,6 +3,8 @@ package com.contafood.service;
 import com.contafood.exception.ResourceNotFoundException;
 import com.contafood.model.Agente;
 import com.contafood.repository.AgenteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.Set;
 
 @Service
 public class AgenteService {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(AgenteService.class);
 
     private final AgenteRepository agenteRepository;
 
@@ -19,24 +23,37 @@ public class AgenteService {
     }
 
     public Set<Agente> getAll(){
-        return agenteRepository.findAll();
+        LOGGER.info("Retrieving the list of 'agenti'");
+        Set<Agente> agenti = agenteRepository.findAll();
+        LOGGER.info("Retrieved {} 'agenti'", agenti.size());
+        return agenti;
     }
 
     public Agente getOne(Long agenteId){
-        return agenteRepository.findById(agenteId).orElseThrow(ResourceNotFoundException::new);
+        LOGGER.info("Retrieving 'agente' '{}'", agenteId);
+        Agente agente = agenteRepository.findById(agenteId).orElseThrow(ResourceNotFoundException::new);
+        LOGGER.info("Retrieved 'agente' '{}'", agente);
+        return agente;
     }
 
     public Agente create(Agente agente){
+        LOGGER.info("Creating 'agente'");
         Agente createdAgente = agenteRepository.save(agente);
+        LOGGER.info("Created 'agente' '{}'", agente);
         return createdAgente;
     }
 
     public Agente update(Agente agente){
+        LOGGER.info("Updating 'agente'");
         Agente updatedAgente = agenteRepository.save(agente);
+        LOGGER.info("Updated 'agente' '{}'", agente);
         return updatedAgente;
     }
 
     public void delete(Long agenteId){
-        agenteRepository.deleteById(agenteId);
+        throw new RuntimeException("SUCA");
+        //LOGGER.info("Deleting 'agente' '{}'", agenteId);
+        //agenteRepository.deleteById(agenteId);
+        //LOGGER.info("Deleted 'agente' '{}'", agenteId);
     }
 }
