e697e6736f8e021ad92831a72232dc96ed68cdd9
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-09-17 13:33:14 +0200

    Fix logging.path property

--- target/contafood-be/WEB-INF/classes/sql/20190819.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `ingrediente` ADD COLUMN unita_di_misura varchar(100);
-ALTER TABLE `ingrediente` ADD COLUMN id_fornitore int(10);
-ALTER TABLE `ingrediente` ADD COLUMN data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `ingrediente` ADD COLUMN attivo bit(1) NOT NULL DEFAULT b'1';
-ALTER TABLE `ingrediente` ADD COLUMN note TEXT;
-
-
-ALTER TABLE `ricetta` MODIFY tempo_preparazione int(10);
