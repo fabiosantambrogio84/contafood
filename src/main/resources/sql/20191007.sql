0a9bc491a27a554a5711570133ad3e240458eebb
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-10-07 17:20:57 +0200

    Add unique index on Fornitore.codice

--- src/main/resources/sql/20191007.sql
+++ src/main/resources/sql/20191007.sql
@@ -2,4 +2,6 @@ ALTER TABLE `fornitore` DROP COLUMN ditta_individuale;
 ALTER TABLE `fornitore` DROP COLUMN nome;
 ALTER TABLE `fornitore` DROP COLUMN cognome;
 
+CREATE UNIQUE INDEX idx_fornitore_codice ON fornitore(codice);
+
 ALTER TABLE `ricetta` CHANGE COLUMN `numero_porzioni` `peso_totale` DECIMAL(10,3) NULL DEFAULT NULL ;
