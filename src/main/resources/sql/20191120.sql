a82707dbee7da5384262ce787c2026b598f7a00e
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-11-20 17:54:51 +0100

    New sql script for step 3 (sconti, categorie articoli, articoli)

---
+++ src/main/resources/sql/20191120.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS `sconto`;
+DROP TABLE IF EXISTS `articolo`;
+DROP TABLE IF EXISTS `categoria_articolo`;
+DROP TABLE IF EXISTS `unita_misura`;
+
+
+CREATE TABLE `unita_misura` (
+	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	nome varchar(255),
+	etichetta varchar(255),
+	data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `categoria_articolo` (
+	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	nome varchar(255),
+	data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `articolo` (
+	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	codice varchar(50),
+	descrizione varchar(255),
+	id_categoria int(10) unsigned,
+	id_fornitore int(10) unsigned,
+	-- `idDestinazione` int(10) unsigned DEFAULT '0',
+	id_aliquota_iva int(10) unsigned,
+	id_unita_misura int(10) unsigned,
+	data date,
+	quantita_predefinita decimal(10,3),
+	prezzo_acquisto decimal(10,3),
+	scadenza_giorni int(10),
+	barcode varchar(255),
+	complete_barcode bit(1) NOT NULL DEFAULT b'0',
+	attivo bit(1) NOT NULL DEFAULT b'1',
+	-- `immagine_1` varchar(500) DEFAULT NULL,
+	-- `immagine_2` varchar(500) DEFAULT NULL,
+	-- `immagine_3` varchar(500) DEFAULT NULL,
+	-- `immagine_4` varchar(500) DEFAULT NULL,
+	-- `immagine_5` varchar(500) DEFAULT NULL,
+	-- `immagine_6` varchar(500) DEFAULT NULL,
+	data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	data_aggiornamento TIMESTAMP,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `fk_articolo_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria_articolo` (`id`),
+	CONSTRAINT `fk_articolo_fornitore` FOREIGN KEY (`id_fornitore`) REFERENCES `fornitore` (`id`),
+	CONSTRAINT `fk_articolo_iva` FOREIGN KEY (`id_aliquota_iva`) REFERENCES `aliquota_iva` (`id`),
+	CONSTRAINT `fk_articolo_unita_misura` FOREIGN KEY (`id_unita_misura`) REFERENCES `unita_misura` (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `sconto` (
+	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	id_cliente int(10) unsigned,
+	id_fornitore int(10) unsigned,
+	id_articolo int(10) unsigned,
+	valore decimal(10,3) NOT NULL DEFAULT '0.000',
+	data_dal date,
+	data_al date,
+	data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `fk_sconto_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+	CONSTRAINT `fk_sconto_fornitore` FOREIGN KEY (`id_fornitore`) REFERENCES `fornitore` (`id`),
+	CONSTRAINT `fk_sconto_articolo` FOREIGN KEY (`id_articolo`) REFERENCES `articolo` (`id`)
+) ENGINE=InnoDB;
+
+
+-- https://www.callicoder.com/spring-boot-file-upload-download-rest-api-example/
