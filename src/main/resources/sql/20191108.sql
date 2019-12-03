68e76ca813781727693c497ff6f65fb0462ecc7a
Fabio Santambrogio <fabio.santambrogio@ext.contactlab.com>
2019-12-03 13:34:41 +0100

    Update docker-test environment

--- src/main/resources/sql/20191108.sql
+++ src/main/resources/sql/20191108.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `produzione` ADD COLUMN data_produzione DATE AFTER codice;
-ALTER TABLE produzione MODIFY COLUMN lotto varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
+ALTER TABLE produzione MODIFY COLUMN lotto varchar(100) NULL;
 ALTER TABLE produzione MODIFY COLUMN lotto_anno int(11) NULL;
 ALTER TABLE produzione MODIFY COLUMN lotto_giorno int(11) NULL;
 ALTER TABLE produzione MODIFY COLUMN lotto_numero_progressivo int(11) NULL;
